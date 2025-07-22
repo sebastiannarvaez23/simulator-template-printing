ab1bd105c078bccb73fb080b06717e44d75cabcd
Sebastian Narvaez <88569352+sebastiannarvaez23@users.noreply.github.com>
2025-07-22 09:20:38 -0500

    Update and rename ViewTemplate.java to TemplateGenerate.java

--- TemplateGenerate.java
+++ TemplateGenerate.java
@@ -5,19 +5,28 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class ViewTemplate {
+public class TemplateGenerator {
 
     private static JTextArea outputTextArea;
 
     public static String processTemplate(String template) {
+    	
         String remplazado1 = template.replace("@{INIT}", "__En columna 1, desde linea 7 letras____\n\n\n\n\n");
+        
         String remplazado2 = remplazado1.replace("@{LF}", "\n");
+        
         String remplazado3 = remplazado2.replace("@{NT(", "");
+        
         String remplazado4 = remplazado3.replace(")}", "");
+        
         String remplazado5 = remplazado4.replace("~", "");
+        
         String remplazado6 = remplazado5.replace("%%COPIA%%", "");
+        
         String remplazado7 = remplazado6.replace("@{CUT(6610", "CORTA");
+        
         return remplazado7;
+        
     }
 
     public static void logError(String message, Exception e) {
@@ -48,9 +57,10 @@ public class ViewTemplate {
     public static void main(String[] args) {
         try {
             JFrame frame = new JFrame("Generador de Templates de Impresion");
-            frame.setSize(400, 700);  
+            frame.setSize(400, 700);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setLayout(new BorderLayout());  
+            frame.setLayout(new BorderLayout());
+            frame.setResizable(false);
 
             JPanel topPanel = new JPanel();
             topPanel.setLayout(new GridLayout(3, 1));
@@ -80,7 +90,21 @@ public class ViewTemplate {
 
             topPanel.add(label);
             topPanel.add(textField);
-            topPanel.add(generateButton);
+
+            JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+            buttonPanel.add(generateButton);
+
+            JButton clearButton = new JButton("Limpiar Template");
+            clearButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    textField.setText("");
+                    outputTextArea.setText("");
+                }
+            });
+            buttonPanel.add(clearButton);
+
+            topPanel.add(buttonPanel);
 
             JPanel outputPanel = new JPanel();
             outputPanel.setLayout(new BorderLayout());
